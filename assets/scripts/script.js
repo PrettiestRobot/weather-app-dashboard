422e95fdcd779347121bc8a44724491ee2f8360c
Thomas Rose-Meyer <prettiest.robot@gmail.com>
2023-04-26 01:47:31 -0400

    api data retrived

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,36 +1,37 @@
-document.querySelector("#searchbar");
+const searchbar = document.querySelector("#searchbar");
+const submitBtn = document.querySelector("#search-btn");
 
 const apiKey = "5801529235171737e0bd3af3fc3c74fc";
-const city = "Seattle";
-longitude = "";
-latitude = "";
 
-// const url = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${apikey}`
-const weatherUrl = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${apiKey}`;
-const geocodeUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city},&limit=1&appid=${apiKey}`;
+let weatherUrl = "";
+let geocodeUrl = "";
 
-fetch(weatherUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+submitBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  let city = searchbar.value;
+  geocodeUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city},&limit=1&appid=${apiKey}`;
+  getWeather();
+});
 
-function getLocation () {
-    fetch(geocodeUrl)
+function getWeather() {
+  fetch(geocodeUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      longitude = data[0].lon;
-      latitude = data[0].lat;
-  
-      console.log(`Longitude: ${longitude}`);
-      console.log(`Latitude: ${latitude}`);
+      let lon = data[0].lon;
+      let lat = data[0].lat;
+      weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+      console.log(`Longitude: ${lon}`);
+      console.log(`Latitude: ${lat}`);
+
+      fetch(weatherUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+        });
     });
 }
-
-getLocation();
-
-
