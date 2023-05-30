0a2817f65dfafb24e4b149e2ee73190d76c89208
Thomas Rose-Meyer <prettiest.robot@gmail.com>
2023-05-29 20:37:07 -0400

    date formatted. City info populating

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,6 +3,8 @@ const submitBtn = document.querySelector("#search-btn");
 const tempCurrent = document.querySelector(".temp-current");
 const windCurrent = document.querySelector(".wind-current");
 const humidityCurrent = document.querySelector(".humidity-current");
+const cityInfo = document.querySelector(".city-info");
+const cityIcon = document.querySelector(".city-info-icon");
 
 
 const apiKey = "5801529235171737e0bd3af3fc3c74fc";
@@ -17,6 +19,15 @@ submitBtn.addEventListener("click", function (event) {
   getWeather();
 });
 
+function formatDate(inputDate) {
+  const date = new Date(inputDate);
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+  const formattedDate = `(${day}/${month.toString().padStart(2, '0')}/${year})`;
+  return formattedDate;
+}
+
 function getWeather() {
   fetch(geocodeUrl)
     .then(function (response) {
@@ -27,15 +38,18 @@ function getWeather() {
       let lat = data[0].lat;
       weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
-      console.log(`Longitude: ${lon}`);
-      console.log(`Latitude: ${lat}`);
-
       fetch(weatherUrl)
         .then(function (response) {
           return response.json();
         })
         .then(function (data) {
           console.log(data);
+          console.log(data.city.name);
+          console.log()
+          let formattedDate = formatDate(data.list[0].dt_txt);
+          cityIcon.src = `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`;
+          let cityText = data.city.name;
+          cityInfo.textContent = `${cityText} ${formattedDate}`;
           tempCurrent.textContent = data.list[0].main.temp + "\u00B0" + "F";
           windCurrent.textContent = data.list[0].wind.speed + " Mph";
           humidityCurrent.textContent = data.list[0].main.humidity +" %";
